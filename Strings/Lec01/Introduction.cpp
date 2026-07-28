a07a337f99629f8d7dd1678984e04aff82628170
Sumit Kumar <sumitknic2006@gmail.com>
2026-07-28 22:24:38 +0530

    introduction_string

---
+++ Strings/Lec01/Introduction.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // char arr[] = {'a','p','p','l','e'};
+
+    // // cout<<arr;
+
+    // for(int i = 0;i<5;i++){
+    //     cout<<arr[i];
+    // }
+
+
+    // Variable size ka banana hai
+
+    // char arr[10]; // index 9 me null character ja rkha hai.
+    // cin>>arr;
+    // arr[2] = '\0';
+    // cout<<arr;
+
+
+    /* Strings :- directly memory ko allocation karti hai.
+    --> isme memory allocation heap me hota hai. */
+
+    // string s = "Sumit";
+    // cout<<s;
+
+    // string s;
+    // cin>>s;
+    // cout<<s;
+
+    // getline
+    // string s;
+    // getline(cin,s);
+    // cout<<s<<endl;
+
+
+    // // Size
+
+    // string s = "Sumit Kumar";
+    // cout<<s.size()<<endl;;
+
+
+    // // combined for the two string that likes for the append operation
+
+    // string s1 = "Sumit" , s2 = "Rohit";
+
+    // string s3 = s1+s2;  // s3 = s1.append(s2);
+
+    // s1 = s1+"pa";
+    // cout<<s3<<endl;
+    // cout<<s1<<endl;
+
+    // // push & pop operation
+
+    // s3.push_back('p');
+    // // s3.pop_back();
+    // cout<<s3;
+
+
+    // string s = "rohit negi is a \"good\" boy";
+    // cout<<s;
+
+    // string s = "\0"; // Homework 
+    // cout<<s;
+
+    string s = "\\0"; // \0
+    cout<<s;
+
+
+
+    
+
+
+
+
+    
+
+}
