4769838852d08d6ee89276a0618eca5469fbe857
Sumit Kumar <sumitknic2006@gmail.com>
2026-07-28 22:31:57 +0530

    reverse_string

---
+++ Strings/Lec01/Reverse_String.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string s = "sumit";
+
+    int start = 0 , end = s.size()-1;
+
+    while(start<end)
+    {
+        swap(s[start],s[end]);
+        start++,end--;
+    }
+    cout<<s;
+}
