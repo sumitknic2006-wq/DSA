3f6c04a0214238c05049c5d0f2cd779265684723
Sumit Kumar <sumitknic2006@gmail.com>
2026-07-28 22:31:31 +0530

    pallindrome_string

---
+++ Strings/Lec01/Pallindrome.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Find to pallindrome
+
+    string s2 = "naman";
+
+    int start = 0 , end = s2.size()-1;
+
+    while(start<end)
+    {
+        if(s2[start]!=s2[end])
+        {
+            cout<<"Not a Pallindrome";
+        }
+        start++ , end--;
+    }
+
+    cout<<"It is a Pallindrome";
+}
